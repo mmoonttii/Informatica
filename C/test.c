73879bd9134dbf34d26b54cb396493b84984cfc6
Monti <lori.monti04@gmail.com>
2022-05-24 15:59:41 +0200

    Fine test quarta

---
+++ C/test.c
@@ -0,0 +1,14 @@
+/* Si scriva un programma per calcolare e^x mediante il suo sviluppo in serie:
+ e^x = 1 + x/1! + x^2/2! + x^3/3! ...
+ Ogni frazione aggiunge precisione al risultato, per cui conviene svilippare la serie fino a un numero di
+ frazioni n adeguatamente elevati, ad esempio compresi tra 30 e 40. Si verifichi che i risultati calcolati in
+ questo modo siano coerenti con quelli forniti dalla funzione intrinseca exp(x) calcolando la differenza
+ dei valori.*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    
+    return 0;
+}
